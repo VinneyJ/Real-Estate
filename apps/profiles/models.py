96d2d287debcfcf0a0dc3037a2d33c452de8838d
VinneyJ <vincentjayden49@gmail.com>
2022-03-23 05:18:45 +0300

    feat: add profile models and signals

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,3 +1,49 @@
+from tabnanny import verbose
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_countries.fields import CountryField
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+from apps.common.models import TimeStampedUUIDModel
+
+User = get_user_model()
+
+
+class Gender(models.TextChoices):
+    MALE = "MALE", _("MALE")
+    FEMALE = "FEMALE", _("FEMALE")
+    OTHER = "OTHER", _("OTHER")
+
+class Profile(TimeStampedUUIDModel):
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
+
+    phone_number = PhoneNumberField(verbose_name=_('Phone number'), max_length=30, default='+41524204242')
+
+    about_me = models.TextField(verbose_name=_('About Me'), default="Say something about yourself...")
+
+    license = models.CharField(verbose_name=_('Real Estate License'), max_length=20, blank=True, null=True)
+
+    profile_photo = models.ImageField(verbose_name=_("Profile photo"), default="/profile_default.png")
+
+    gender = models.CharField(verbose_name=_('Gender'), choices=Gender.choices, default=Gender.OTHER, max_length=15)
+
+    country = CountryField(verbose_name=_('Country'), default="KE", blank=False, null=False)
+
+    city = models.CharField(verbose_name=_('City'), max_length=180, default='Nairobi', blank=False, null=False)
+
+    is_buyer = models.BooleanField(verbose_name=_("Buyer"), default=False, help_text=_('Are you looking to Buy a Property?'))
+
+    is_seller = models.BooleanField(verbose_name=_('Seller'), default=False, help_text=_('Are you looking to Sell a Property?'))
+
+    is_agent = models.BooleanField(verbose_name=_('Agent'), default=False, help_text=_('Are you an agent?'))
+
+    top_agent = models.BooleanField(verbose_name=_('Top Agent'), default=False)
+
+    rating = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+
+    num_reviews = models.IntegerField(verbose_name=_('Number of reviews'), default=0, null=True, blank=True)
+
+
+    def __str_(self):
+        return f"{self.user.username}'s profile"
