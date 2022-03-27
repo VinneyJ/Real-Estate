74d9f755143b1a51687d0d836cf6193f9da25906
VinneyJ <vincentjayden49@gmail.com>
2022-03-27 21:53:48 +0300

    feat: user auth-login, register, pass confirm, reset pass

---
+++ apps/users/serializers.py
@@ -0,0 +1,57 @@
+from dataclasses import field
+from importlib.abc import ExecutionLoader
+from django.contrib.auth import get_user_model
+
+from django_countries.serializer_fields import CountryField
+from djoser.serializers import UserCreateSerializer
+from phonenumber_field.serializerfields import PhoneNumberField
+from rest_framework import serializers
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    gender = serializers.CharField(source='profile.gender')
+    phone_number = PhoneNumberField(source='profile.phone_number')
+    profile_photo = serializers.ImageField(source='profile.profile_photo')
+    country = CountryField(source='profile.country')
+    city = serializers.CharField(source='priofile.city')
+    top_seller = serializers.BooleanField(source='profile.top_seller')
+    first_name = serializers.SerializerMethodField()
+    last_name = serializers.SerializerMethodField()
+    full_name = serializers.SerializerMethodField(source='get_full_name')
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'first_name', 
+            'last_name',
+            'profile_photo', 
+            'full_name', 
+            'email', 
+            'gender', 
+            'phone_number', 
+            'country', 
+            'city', 
+            'top_seller'
+            ]
+
+    def get_first_name(self, obj):
+        return obj.first_name.title()
+
+    def get_last_name(self, obj):
+        return obj.last_name.title()
+
+    def to_representation(self, instance):
+        representation = super(UserSerializer, self).to_representation(instance)
+
+        if instance.is_superuser:
+            representation['admin'] = True
+        return representation
+
+
+class CreateUserSerializer(UserCreateSerializer ):
+    class Meta(UserCreateSerializer.Meta):
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'password']
--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -1,5 +1,18 @@
 from .base import *
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_USE_TLS = True
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = 'info@real-estate1.com'
+DOMAIN = env('DOMAIN')
+
+SITE_NAME = 'Real Estate'
+
+
+
 DATABASES = {
     "default": {
         "ENGINE": env("POSTGRES_ENGINE"),
